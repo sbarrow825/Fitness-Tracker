f26faadf1b68697e6a83186e322f011f79cb229e
sbarrow825 <66639249+sbarrow825@users.noreply.github.com>
2020-08-07 21:28:23 -0700

    set up rough draft of routes

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  exercises: {
-    type: Array,
-    default: []
-  },
-  day: {
-      type: Date,
-      default: Date.now
-  },
-  duration: {
-      type: Number,
-      default: 0
-  }
+    exercises: {
+        type: Array,
+        default: []
+    },
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    duration: {
+        type: Number,
+        default: 0
+    }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,29 +1,23 @@
-const Workout = require("./Workout.js")
+const db = require("../models")
 const Mongoose  = require("mongoose")
 
 
 module.exports = function (app) {
 
-    app.get("/api/workouts", async (req, res) => {
-        var workouts = await Workout.find()
-        for (const workout of workouts) {
-            await workout.setTotalDuration()
-        }
-        res.json(workouts)
+    app.get("/api/workouts", (req, res) => {
+        res.json(db.Workout.find());
+    });
 
-    })
-
-    app.post("/api/workouts", (req, res) => {
-        Workout.create(req.body).then(data => {
+    app.post("/api/workouts", ({body}, res) => {
+        db.Workout.create(body).then(data => {
             res.json(data)
-        })
-    })
+        });
+    });
 
-    app.put("/api/workouts/:id", async (req, res) => {
-        var workoutId = req.params.id
-        Workout.findOneAndUpdate({ _id: Mongoose.Types.ObjectId(workoutId) }, {
+    app.put("/api/workouts/:id", async ({body}, res) => {
+        db.Workout.findOneAndUpdate({ _id: Mongoose.Types.ObjectId(req.params.id) }, {
             $push: {
-                exercises: req.body
+                exercises: body
             }
         }).then(data => {
             res.json(data)
@@ -32,28 +26,6 @@ module.exports = function (app) {
     })
 
     app.get("/api/workouts/range", (req, res) => {
-        Workout.find().then(data => {
-            res.json(data)
-        })        
+        res.json(db.Workout.find());   
     })
-
-    function findRange(workouts) {
-        workoutRange = []
-        const emptyWorkout = {
-            exercises : [],
-            totalDuration: 0
-        }
-        var dayOfWeek = new Date(Date.now()).getDay()
-        console.log
-        for (i=0; i<= dayOfWeek; i++) {
-            if (workouts[i]) {
-                workoutRange.unshift(workouts[i])
-            }
-            else {
-                workoutRange.unshift(emptyWorkout)
-            }
-        }
-        console.log(workoutRange)
-        return workoutRange
-    }
 }
